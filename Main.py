6f94779775b3a4268e15a86e8f7c3f005e018188
AnaelGor <anaellegorfinkel@gmail.com>
2018-11-03 14:19:42 +0200

    json

--- Main.py
+++ Main.py
@@ -1,20 +1,35 @@
 import os
 import pathlib
 import time
-
+import readJson
 import ReadFile
-
+import json
+import requests
+import string
 rootdir = "C:\Retrieval_folder\corpus"
 
 
 
+from pprint import pprint
+
+
 def main():
+    #''''
     start = time.time()
-    rf = ReadFile.ReadFile(rootdir)
+    rf = readJson.ReadJson(rootdir)
     rf.create_maagar_meida()
     end = time.time()
 
     print (end-start)
+
+    #
+    # path=rootdir + "\\allDocs"
+    # file = open (path,'r')
+    # lines = file.readlines()
+    # file.close()
+    # for line in lines:
+    #     print (line)
+    # print ("c")
 
 
 main()
