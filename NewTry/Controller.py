ffd06718822812a70ac3ccb4e6c26f4e373c6b1c
Maor Elfassy <maorelf@post.bgu.ac.il>
2018-11-23 20:07:16 +0200

    added stemmer. still not working. I will edit it.

--- NewTry/Controller.py
+++ NewTry/Controller.py
@@ -6,12 +6,13 @@ from fractions import Fraction
 from NewTry import ReadFile
 from NewTry import Parser
 from NewTry.ReadFile import dic_to_parse
+from NewTry import PorterStemmer
 
+stemmer = PorterStemmer.PorterStemmer()
 
 
 def SendToParser(file):
-    one_file_dic = Parser.parse(dic_to_parse, file)
-    pass
+    return Parser.parse(dic_to_parse, file)
 
 
 def data_set_Path(path):
@@ -20,10 +21,31 @@ def data_set_Path(path):
     Parser.set_stop_words_file(__stopwords_path)
 
 
+def getStemmerFromUser(flag):
+
+    #TODO: implements this from user GUI
+    return flag
+
+
+# def stem_dictionary(dictionary_of_one_file,stemmer):
+#     new_dic = {}
+#     for doc in dictionary_of_one_file:
+#         for term, freqInDoc in dictionary_of_one_file[doc].items():
+#             stemmed_term = stemmer.stem(term)
+#             if (stemmed_term!=term): #if the stem has changed the term
+#                 dictionary_of_one_file[doc].pop(term)
+#                 if stemmed_term in dictionary_of_one_file[doc]: #if the stemmed term is already on this dictionary
+#                     dictionary_of_one_file[doc][stemmed_term] += freqInDoc
+#                 else:
+#                     dictionary_of_one_file[doc][stemmed_term] = freqInDoc
+#     return dictionary_of_one_file
+#
+
+
 def Main():
+    global stemmer
     path = 'C:\Retrieval_folder\corpus'
     start = time.time()
-    global corpus_path
     corpus_path = path
     data_set_Path(path)
     #counter = 0
@@ -33,10 +55,14 @@ def Main():
         for file in files:
 
             if str(file) != 'stop_words.txt':
+                start = time.time();
                 ReadFile.takeDocsInfoFromOneFile(str(pathlib.PurePath(root, file)))
-                SendToParser(file)
+                dictionary_of_one_file = SendToParser(file)
                 dic_to_parse.clear()
-                #counter = counter + 1
+                if getStemmerFromUser(True) == True:
+                    dictionary_of_one_file = stem_dictionary(dictionary_of_one_file,stemmer)
+                counter = counter + 1
+                print("" +str(counter) + ": " + str(time.time()-start))
 
     #saveDictionaryToDisk()
     end2 = time.time();
